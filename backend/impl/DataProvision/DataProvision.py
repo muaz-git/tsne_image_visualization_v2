77a7744f59f629a1ae7acd928b5a48c9d41b0e60
Sven Stauden <sven.stauden@dfki.de>
2017-10-27 16:24:16 +0200

    Initialized repository with empty project files and first abstract classes

---
+++ backend/impl/DataProvision/DataProvision.py
@@ -0,0 +1,12 @@
+
+
+class DataProvider:
+
+    def __init__(self):
+        pass
+
+    def process_single_image(self, image_file):
+        pass
+
+    def pass_data(self, amount):
+        pass
---
+++ backend/impl/DimensionalityReduction.py
@@ -0,0 +1,13 @@
+
+
+class DimReducer:
+
+    def __init__(self):
+        pass
+
+    def reduce_dimensions(self, dim_count):
+        pass
+
+class TSNEReducer(DimReducer):
+
+    def __init__(self):
